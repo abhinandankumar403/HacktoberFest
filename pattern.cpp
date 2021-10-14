b6d13e2bb64cfd9eb66c4a539d096b1405dedcd3
sharmaarti <92197509+sharmaarti@users.noreply.github.com>
2021-10-14 14:43:44 +0530

    Add files via upload

--- pattern.cpp
+++ pattern.cpp
@@ -1,23 +1,20 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-int main(){
-    int rows, col;
-    cout<<"enter the no. of rows:\n";
-    cin>>rows;
-    cout<<"enter the no. of col :\n";
-    cin>>col;
-
-    for(int i=1;i<=rows; i++){
-        for (int j=1; j<=col;j++){
-            if (i==1 || i==rows || j== 1|| j==col){
-                cout<<"*";
-            } else {
-                cout<<" ";
-            }
-        }
-        cout<<endl;
-    }
-    return 0;
+int main()
+{
+int n, s, i, j;
+cout << "Enter number of rows: ";
+cin >> n;
+for(i = n; i >= 1; i--)
+{
+//for loop to put space
+for(s = i; s < n; s++)
+cout << " ";
+//for loop for displaying star
+for(j = 1; j <= i; j++)
+cout << "* ";
+// ending line after each row
+cout << "\n";
+}
+return 0;
 }
